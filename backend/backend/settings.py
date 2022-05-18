e8b9dfa122a1afcd791e6ed01ceab5dba53c47e4
msobczyk-x <75531536+msobczyk-x@users.noreply.github.com>
2022-05-18 13:15:24 +0200

    Update settings.py
    Ignore secret key leak

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -19,6 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
+
+#It is only for educational purposes/It won't be used in production
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-jpi9^zj_hzj9ie!e(%$m&akzqdvwf#-9d3@9y+)v&c#+s$c05j'
 
